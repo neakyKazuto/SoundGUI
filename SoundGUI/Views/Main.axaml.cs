a5ad96e0bc32e875c80fa8133b96e8fdd853fd24
Htomsik <JessJake@proton.me>
2023-05-21 22:44:57 +0300

    Add Main view

---
+++ SoundGUI/Views/Main.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SoundGUI.Views;
+
+public partial class Main : UserControl
+{
+    public Main()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
