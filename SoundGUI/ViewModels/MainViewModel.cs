23670f82230cbaed5106a1f65d48105a64b28af1
Htomsik <JessJake@proton.me>
2023-05-24 18:58:09 +0300

    Add sound commands

--- SoundGUI/ViewModels/MainViewModel.cs
+++ SoundGUI/ViewModels/MainViewModel.cs
@@ -1,11 +1,47 @@
-﻿using ReactiveUI.Fody.Helpers;
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using SoundGUI.Extensions;
+using SoundGUI.Services;
 using SoundGUI.ViewModels.Base;
 
 namespace SoundGUI.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    [Reactive]
-    public string Text { get; set; }
+    #region Properties
+
+    [Reactive] public string Text { get; set; } = "";
+
+    #endregion
     
+    #region Fields
+
+    private readonly ISoundService _soundService;
+
+    #endregion
+
+    #region Constructors
+    public MainViewModel()
+    {
+        _soundService = new SoundService();
+        
+        Start = ReactiveCommand.CreateFromObservable(()=>
+            Observable.StartAsync(ct => _soundService.PlayTimes(Text.LineCount(),ct)).TakeUntil(Cancel));
+        
+        Cancel = ReactiveCommand.Create(() => { }, CanCancel);
+    }
+    #endregion
+    
+    #region Commands
+    
+    public IReactiveCommand Start { get; }
+    
+    public ReactiveCommand<Unit, Unit> Cancel { get; }
+
+    private IObservable<bool> CanCancel => this.WhenAnyObservable(x => x.Start.IsExecuting);
+    
+    #endregion
 }
