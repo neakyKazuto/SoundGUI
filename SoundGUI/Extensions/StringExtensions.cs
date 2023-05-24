3723e782067f9da648e6a93bbbba5cc4852c9dc7
neakyKazuto <gamevrimed@gmail.com>
2023-05-24 18:47:11 +0300

    Add LineCount to Extensions

---
+++ SoundGUI/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace SoundGUI.Extensions;
+
+public static class StringExtensions
+{
+    public static int LineCount(this string LineC)
+    {
+        return LineC.Split('\n').Length;
+    }
+}
